1fc4eca21604a206ace7b23666c6f7aa42916add
Christopher Cruzen <christopher.a.cruzen@gmail.com>
2020-05-12 18:39:32 -0500

    Add Drawing to SplinePanel

--- src/ui/SplinePanel.java
+++ src/ui/SplinePanel.java
@@ -1,9 +1,8 @@
 package ui;
 
-import com.sun.scenario.animation.SplineInterpolator;
-
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Line2D;
 
 /* Spline Interpolator
  * Christopher Cruzen
@@ -14,23 +13,41 @@ import java.awt.*;
  * display.
  */
 
-public class SplinePanel extends JPanel {
+class SplinePanel extends JPanel {
 
 
     /*--- Variable Declarations ---*/
 
-    
+    private static final int LINE_WIDTH = 2;
+    private static final Color LINE_COLOR = Color.GRAY;
 
 
     /*--- Constructor ---*/
 
-    public SplinePanel() {
+    SplinePanel() {
         this.setBackground(Color.WHITE);
     }
 
 
     /*--- Draw Method ---*/
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
-    /*--- Private Methods ---*/
+        // Setup 2D Graphics
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setColor(LINE_COLOR);
+        graphics.setStroke(new BasicStroke(LINE_WIDTH));
+
+        // Calculate Draw Variables
+        int width = this.getSize().width;
+        int height = this.getSize().height;
+        Point lineStart = new Point(100, height / 2);
+        Point lineEnd = new Point(width - 100, height / 2);
+
+        // Draw Line
+        Line2D line = new Line2D.Float(lineStart.x, lineStart.y, lineEnd.x, lineEnd.y);
+        graphics.draw(line);
+    }
 }
